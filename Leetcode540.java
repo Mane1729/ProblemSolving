beeb7f10ad9c605dc50a22e366ad68e444e794cd
Mane Darbinyan <72979606+Mane1729@users.noreply.github.com>
2022-12-14 16:32:52 +0400

    remove additional variables

--- Leetcode540.java
+++ Leetcode540.java
@@ -1,40 +1,33 @@
 public class Leetcode540 {
-    public int singleNonDuplicate(int[] nums) {
-        if(nums.length == 1){
+    if(nums.length == 1){
             return nums[0];
         }
 
-        int left = 0;
+        int left = 0; 
         int right = nums.length-1;
         int mid = 0;
-        boolean isEven;
-        int midValue = 0;
 
         while(left < right){
             mid = (left + right) / 2;
-            isEven = mid % 2 == 0;
-            midValue = nums[mid];
 
-            if(isEven && midValue == nums[mid+1]){
+            if(mid % 2 == 0 && nums[mid] == nums[mid+1]){
                 left = mid + 2;
             }
-            else if(isEven && midValue == nums[mid-1]){
+            else if(mid % 2 == 0 && nums[mid] == nums[mid-1]){
                 right = mid - 2;
             }
-            else if(!isEven && midValue == nums[mid+1]){
+            else if(!(mid % 2 == 0) && nums[mid] == nums[mid+1]){
                 right = mid - 1;
             }
-            else if(!isEven && midValue == nums[mid-1]){
+            else if(!(mid % 2 == 0) && nums[mid] == nums[mid-1]){
                 left = mid + 1;
             }
             else{
-                return midValue;
+                return nums[mid];
             }
         }
 
-        // the case when left == right
         return nums[left];
-
     }
 }
 
