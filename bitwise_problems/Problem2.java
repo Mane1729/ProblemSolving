6c43350517cad14f9ce343539e36e80df83c4432
Mane Darbinyan <72979606+Mane1729@users.noreply.github.com>
2022-12-15 16:24:51 +0400

    better writing version

--- bitwise_problems/Problem2.java
+++ bitwise_problems/Problem2.java
@@ -4,16 +4,11 @@ package bitwise_problems;
 
 public class Problem2 {
     public static void evenOrOddOneBits(int n){
-        // first division - 16 bits
-        n ^= n >> 16;
-        // 8 bits
-        n ^= n >> 8;
-        // 4 bits
-        n ^= n >> 4;
-        // 2 bits
-        n ^= n >> 2;
-        // 1 bit
-        n ^= n >> 1;
+        int half = 16;
+		while(half > 0){
+		    n ^= n >> half;
+		    half /= 2;
+		}
 
         if((n & 1) == 1){
             System.out.println("Odd");
